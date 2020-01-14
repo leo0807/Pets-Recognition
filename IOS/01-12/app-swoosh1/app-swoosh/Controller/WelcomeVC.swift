152aca31b855a6d3bb2ec6e792c61f7ea9c823d1
Scott ZHANG <41654335+leo0807@users.noreply.github.com>
2020-01-14 13:39:53 +1100

    Add files via upload

---
+++ IOS/01-12/app-swoosh1/app-swoosh/Controller/WelcomeVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by Junxu ZHANG on 9/1/20.
+//  Copyright © 2020 Junxu ZHANG. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeVC: UIViewController {
+    @IBOutlet weak var swoosh: UIImageView!
+    
+    @IBOutlet weak var bgImg: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue){
+        
+    }
+
+}
+
