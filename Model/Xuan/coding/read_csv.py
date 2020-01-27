4466b975363ae3d32ca9784c087550de10818ea6
Derekxuan <derekxuan@outlook.com>
2020-01-27 16:43:19 +1100

    fix training data

--- Model/Xuan/coding/read_csv.py
+++ Model/Xuan/coding/read_csv.py
@@ -1,7 +1,23 @@
+import glob
+import os
 import pandas as pd
 
-csv1Path = 'result.csv'
-data = pd.read_csv(csv1Path)
-for index, rows in data.iterrows():
-    print(len(rows['pixel']))
+csv1Path = 'prep_images_rotated.csv'
+csv2Path = 'result.csv'
+
+inputfile = "*.csv"
+outputfile = "dog_training.csv"
+csv_list = glob.glob(inputfile)
+
+filepath = csv_list[0]
+df = pd.read_csv(filepath)
+df = df.to_csv(outputfile, index=False)
+
+for i in range(1, len(csv_list)):
+    filepath = csv_list[i]
+    df = pd.read_csv(filepath)
+    df = df.to_csv(outputfile, index=False, header=False, mode='a+')
+
+
+
 
