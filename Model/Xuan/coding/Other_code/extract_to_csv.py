0e69be6371852b27c78abf7ee95af45fd0eab4c1
Derekxuan <derekxuan@outlook.com>
2020-02-10 13:53:04 +1100

    refact code

--- Model/Xuan/coding/Other_code/extract_to_csv.py
+++ Model/Xuan/coding/Other_code/extract_to_csv.py
@@ -17,8 +17,8 @@ import pandas as pd
 import shutil
 
 # image size for prediction
-img_width = 100
-img_height = 100
+img_width = 160
+img_height = 160
 # scale factor for preprocessing
 picSize = 320
 rotation = True
@@ -35,13 +35,8 @@ pathCat = "haarcascade_frontalcatface.xml"
 faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + pathCat)
 detector = dlib.cnn_face_detection_model_v1(pathDet)
 
-# landmarks detector
-pathPred = '../faceDetectors/landmarkDetector.dat'
-predictor = dlib.shape_predictor(pathPred)
-
 # helper class
 helper = No_Preprocessing(img_width, img_height)
-
 
 
 # ------------ find the label ------------
@@ -61,8 +56,8 @@ def find_label(path):
 
 # ----------------------------------------
 
-def dog_preprocess(path, savePath):
-    # read image from path
+
+def preprocess(path, savePath):
     orig = cv2.imread(path)
     dirpath, filedir = os.path.split(path)
     filename, extend = os.path.splitext(filedir)
@@ -73,83 +68,6 @@ def dog_preprocess(path, savePath):
         ratio = picSize / height
         image = cv2.resize(orig, None, fx=ratio, fy=ratio)
 
-        # color gray
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-        # detect face(s)
-        dets = detector(gray, upsample_num_times=1)
-        imageList = []  # for return
-        for i, d in enumerate(dets):
-            # save coordinates
-            x1 = max(int(d.rect.left() / ratio), 1)
-            y1 = max(int(d.rect.top() / ratio), 1)
-            x2 = min(int(d.rect.right() / ratio), width - 1)
-            y2 = min(int(d.rect.bottom() / ratio), height - 1)
-
-            # detect landmarks
-            shape = face_utils.shape_to_np(predictor(gray, d.rect))
-            points = []
-            index = 0
-            for (x, y) in shape:
-                x = int(round(x / ratio))
-                y = int(round(y / ratio))
-                index = index + 1
-                if index == 3 or index == 4 or index == 6:
-                    points.append([x, y])
-            points = np.array(points)  # right eye, nose, left eye
-
-            # rotate
-            if rotation == True:
-                xLine = points[0][0] - points[2][0]
-                if points[2][1] < points[0][1]:
-                    yLine = points[0][1] - points[2][1]
-                    angle = math.degrees(math.atan(yLine / xLine))
-                else:
-                    yLine = points[2][1] - points[0][1]
-                    angle = 360 - math.degrees(math.atan(yLine / xLine))
-                rotated = imutils.rotate(orig, angle)
-                # detectFace(rotated, picSize)
-
-            # highlight face and landmarks
-            # cv2.imwrite(savePath + os.sep + filename + '.jpg', orig)
-            # cv2.polylines(orig, [points], True, (0, 255, 0), 1)
-            # cv2.rectangle(orig, (x1, y1), (x2, y2), (255, 0, 0), 1)
-            imageList.append(orig)
-            print(x1, x2, y1, y2)
-
-            # prepare for prediction
-            little = cv2.resize((rotated[y1:y2, x1:x2]), (img_width, img_height))  # crop and resize
-            pixel = cv2.cvtColor(little, cv2.COLOR_BGR2GRAY)
-            to_csv_data = ' '.join(map(str, pixel.flatten()))
-            x = np.expand_dims(pixel, axis=0)
-            x = x.reshape((-1, 100, 100, 1))
-            imageList.append(x)
-            return imageList, to_csv_data  # order: marked picture, input for classifier
-    return None
-
-
-def cat_preprocess(path, savePath):
-    orig = cv2.imread(path)
-    dirpath, filedir = os.path.split(path)
-    filename, extend = os.path.splitext(filedir)
-
-    if not orig is None:
-        # resize
-        height, width, channels = orig.shape  # read size
-        ratio = picSize / height
-        image = cv2.resize(orig, None, fx=ratio, fy=ratio)
-
-        # color gray
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-        # detect face(s)
-        faces = faceCascade.detectMultiScale(
-            gray,
-            scaleFactor=1.02,
-            minNeighbors=3,
-            minSize=(50, 50),
-            flags=cv2.CASCADE_SCALE_IMAGE
-        )
         imageList = []  # for return
         # t = datetime.datetime.now()
         for (i, (x, y, w, h)) in enumerate(faces):
@@ -173,16 +91,6 @@ def cat_preprocess(path, savePath):
 # writeImage2csv("..\\Data for project\\cat\\cat_angry", "..\\test.csv")
 
 
-
-# imageList, csv_data = preprocess(testpath)
-# images = [['emotion', 'pixel'], ["1", csv_data]]
-# images = [find_label(testpath), preprocess(testpath)[0][1]]
-# print(len(preprocess(testpath)[0][1]))
-
-
-# data = pd.read_csv('../prep_images_rotated.csv')
-# # data = data.sample(frac=1).reset_index(drop=True)
-# print(data)
 
 # --------define dog or cat to classify--------
 classify = 'dog'
@@ -226,7 +134,6 @@ for folders in os.listdir(folderPath):
 #             pixel = pixels
 #             images.append([label, pixel])
 
-# csvPath = '../'
 csvPath = '..\\result_' + classify + '_V3' + '.csv'
 if images is not None and images != []:  # found face on image
     images = pd.DataFrame(images)
